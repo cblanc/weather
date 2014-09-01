1194a17db0317d5af6fa01a14eae329859f5a337
cblanc <cablanchard@gmail.com>
2014-09-01 22:36:25 +0100

    Readme Retrieves location information

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weather
+
+#GETIP
+
+Utility to retrieve 5 day weather forecast for a location via [http://openweathermap.org/](http://openweathermap.org/)
+
+## Usage
+
+Add binary to $PATH
+
+```
+$ weather london
+
+or
+
+$ weather london,uk
+```
--- weather.go
+++ weather.go
@@ -3,19 +3,56 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+// type dailyForecast {
+
+// }
+
 type forecast struct {
+	City struct {
+		Name  string
+		Coord struct {
+			Lon float64
+			Lat float64
+		}
+	}
+	// List []dailyForecast
 }
 
 func main() {
+	var location string
+
 	if len(os.Args) < 2 {
-		fmt.Printf("Please provide a location for a weather forecast")
+		fmt.Printf("Please provide a location for a weather forecast. E.g. $ weather london")
+		os.Exit(1)
 	} else {
 		location = os.Args[1]
 		fmt.Printf("Looking up: %s\n", location)
 	}
+
+	response, err := http.Get("http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=5&q=" + location)
+
+	defer response.Body.Close()
+
+	if err != nil {
+		fmt.Printf("Unable to retrieve data due to error: %s", err)
+		response.Body.Close()
+		os.Exit(1)
+	}
+
+	var f forecast
+
+	if err := json.NewDecoder(response.Body).Decode(&f); err != nil {
+		fmt.Printf("Unable to parse result due to error: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Location: %s\n", f.City.Name)
+	fmt.Printf("Geolocation: %.5f,%.5f\n", f.City.Coord.Lon, f.City.Coord.Lat)
+
+	os.Exit(0)
+
 }

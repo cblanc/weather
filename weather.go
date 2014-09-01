aa9af5f6497e50b32802cc5d394136b531032b41
cblanc <cablanchard@gmail.com>
2014-09-01 23:14:56 +0100

    Pulls 5 day forecast

--- weather.go
+++ weather.go
@@ -5,21 +5,37 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-// type dailyForecast {
+type dailyForecast struct {
+	Dt   int64
+	Temp struct {
+		Min float32
+		Max float32
+		Day float32
+	}
+	Weather  []weatherDescription
+	Pressure float32
+	Humidity int16
+	Clouds   int16
+	Speed    float32
+}
 
-// }
+type weatherDescription struct {
+	Main        string
+	Description string
+}
 
 type forecast struct {
 	City struct {
 		Name  string
 		Coord struct {
-			Lon float64
-			Lat float64
+			Lon float32
+			Lat float32
 		}
 	}
-	// List []dailyForecast
+	List []dailyForecast
 }
 
 func main() {
@@ -50,8 +66,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Location: %s\n", f.City.Name)
-	fmt.Printf("Geolocation: %.5f,%.5f\n", f.City.Coord.Lon, f.City.Coord.Lat)
+	fmt.Printf("5 day forecast for %s (%.5f, %.5f)\n\n", f.City.Name, f.City.Coord.Lon, f.City.Coord.Lat)
+
+	for _, daily := range f.List {
+		fmt.Printf("Date: %s\n", time.Unix(daily.Dt, 0).Format(time.RFC850))
+		fmt.Printf("Description: %s (%s)\n", daily.Weather[0].Main, daily.Weather[0].Description)
+		fmt.Printf("Temperate (Range): %0.fC (%.0fC-%.0fC)\n", daily.Temp.Day, daily.Temp.Min, daily.Temp.Max)
+		fmt.Printf("Cloud Coverage: %d%%\n", daily.Clouds)
+		fmt.Printf("\n")
+	}
 
 	os.Exit(0)
 
